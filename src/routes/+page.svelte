6833c3081a233dae68851b8975007c4cb93e35a9
Tony Qing <tonyqing2022@gmail.com>
2023-04-27 21:20:26 -0500

    Finished Coloring and Adding Visuals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,22 +13,27 @@
 
     let value: number;
     const steps = [
-      "<p>Last year in Chicago there were over .</p>",
+      "<p>As one of America's largest and most diverse cities, Chicago has long been a hub of activity for both residents and tourists alike. Unfortunately, it has also been a hotbed for theft and motor theft, with many citizens falling victim to these crimes every year.</p><p> In this data visualization, we will examine the prevalence and trends of theft and motor theft in Chicago, shedding light on this important issue and providing insights into the challenges faced by law enforcement and communities in combating these crimes.</p>",
       "<p>The scatterplot uses tweened values to automatically update your points with smooth transitions. It also binds to the width of the container <code>div</code>, so its responsive by default.</p>",
       "<p>Try resizing me to see the 'side-by-side' version, compared to the 'text-on-top' version that appears on small screens.</p><p>Want it to always appear 'text-on-top'? Just comment out the media query at the bottom of our styles (as in, leave the styles but comment out the surrounding <code>media</code> query).</p>",      "<p>Last year in Chicago there were over .</p>",
       "<p>The scatterplot uses tweened values to automatically update your points with smooth transitions. It also binds to the width of the container <code>div</code>, so its responsive by default.</p>",
       "<p>Try resizing me to see the 'side-by-side' version, compared to the 'text-on-top' version that appears on small screens.</p><p>Want it to always appear 'text-on-top'? Just comment out the media query at the bottom of our styles (as in, leave the styles but comment out the surrounding <code>media</code> query).</p>",
     ];
-    kde_motor_theft.features.sort((d, k) => d.geometry.coordinates[0].length - k.geometry.coordinates[0].length) 
-    kde_theft.features.sort((d, k) => d.geometry.coordinates[0].length - k.geometry.coordinates[0].length)
 
+    let tick = 0;
+    setInterval(() => {
+      tick += 1;
+    }, 3000)
     const height = 800;
     const width = 800;
     const viewBox = `0 0 ${height} ${width}`;
-    let projection: d3.GeoProjection;
+    let projection: d3.GeoProjection = d3.geoMercator().scale((100 * width)).center([-87.6598, 41.9081]);;
+    $: console.log(projection);
+    $: tick = tick % kde_motor_theft.features.length;
     $: pathGenerator = d3.geoPath(projection);
     onMount(() => {
-      projection = d3.geoMercator().scale((100 * width)).center([-87.6598, 41.9081])
+      projection = d3.geoMercator().scale((100 * width)).center([-87.6598, 41.9081]);
+      pathGenerator = d3.geoPath(projection);
     })
 </script>
 <section >
@@ -49,28 +54,34 @@
   <div class="map-container sticky">
     <svg {viewBox} >
         <clipPath id="clip-path">
-          <path d={pathGenerator(chicago_outline.features[0])} fill="none" stroke="black" stroke-width="1px"></path>
+          <path d={pathGenerator(chicago_outline.features[0])} fill="white" stroke="black" stroke-width="1px"></path>
         </clipPath>
-        <g class="map">
+        <rect clip-path="url(#clip-path)" fill="white" height="100%" width="100%"></rect>
+        <g >
+          {#each chicagodata.features as d, i}
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          {console.log(parseInt(d.properties.shape_area) * 0.00000000268936051)}
+            <path d={pathGenerator(d)} fill={value === 0 ? "white" : 
+                                                              value === 1 || value === 2 ? `rgba(244, 15, 151, ${(d.properties.num_motor_thefts ?? 0) / (parseInt(d.properties.shape_area) * 0.00000000268936051 + .1)})` :
+                                                              value === 3 || value === 4 ? `rgba(244, 15, 151, ${(d.properties.num_thefts ?? 0) / (parseInt(d.properties.shape_area) * 0.00000000268936051 + .1) })` : "none"} stroke="black" on:click={(event) => {console.log(d)}}></path>
           
+          {/each}
+        </g>
+        <g class="map">
           {#each kde_motor_theft.features as d, i}
-            <path clip-path="url(#clip-path)"  id={`mt-${i}`} d={pathGenerator(d)} opacity={value === 3 ? 1 : 0} fill={`rgba(${45 * (i)},${15 * i},${5 * i},.25)`} stroke="black"></path>          
-          {/each}
+          <path clip-path="url(#clip-path)" id={`mt-${i}`} d={pathGenerator(d).slice(0, pathGenerator(d).indexOf('Z'))} opacity={value === 2 ? 1 : 0}  fill={`rgba(${(i + 1) * 15},19,59,${(i + 1) * i * 0.055})`} stroke="black"></path>          
+            {/each}
           {#each kde_theft.features as d, i}
-            <path clip-path="url(#clip-path)" id={`t-${i}`} d={pathGenerator(d)} opacity={value === 4 ? 1 : 0} fill={`rgba(${45 * (i)},${15 * i},${1 * i}, .25)`} stroke="black"></path>
+            <path clip-path="url(#clip-path)" id={`t-${i}`} d={pathGenerator(d).slice(0, pathGenerator(d).indexOf('Z'))} opacity={value === 4 ? 1 : 0} fill={`rgba(${(i + 1) * 15},19,59,${(i + 1) * i * 0.055})`} stroke="black"></path>
           {/each}
-
         </g>
         <g >
           {#each chicagodata.features as d, i}
           <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <path id={`clipPath`} d={pathGenerator(d)} fill={value === 0 ? "white" : 
-                                                              value === 1 ? `rgba(244, 15, 151, ${d.properties.num_motor_thefts})` :
-                                                              value === 2 ? `rgba(244, 15, 151, ${d.properties.num_thefts})` : "rgba(255,255,255,.1)"} stroke="black" on:click={(event) => {console.log(d.properties.pri_neigh)}}></path>
+            <path d={pathGenerator(d)} fill={"none"} stroke="black" on:click={(event) => {console.log(d)}}></path>
           
           {/each}
         </g>
-
     </svg>
   </div>
 </div>
@@ -85,6 +96,7 @@
   :global(body) {
     height: 300vh;
     font-family: "Comic Sans MS";
+    background-color: blanchedalmond;
   }
 
   path {
@@ -154,7 +166,7 @@
     width: 100%;
     margin-top: 1em;
     text-align: center;
-    transition: background 100ms;
+    transition: background 500ms;
     display: flex;
   }
   .steps-container {
@@ -165,12 +177,14 @@
 		background: white;
 		color: black;
 	}
-  
+
   @media screen and (max-width: 768px) {
     .section-container {
+      align-items: center;
       flex-direction: column-reverse;
     }
     .sticky {
+      top: 25%;
       width: 95%;
 			margin: auto;
     }
